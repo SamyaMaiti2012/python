c262994fc50caabcaaad382542b4104d552ee43f
Samya Maiti <samya.maiti2012@gmail.com>
2018-06-26 00:16:09 +0530

    Implemented logic

--- hackerrank/simpleArraySum.py
+++ hackerrank/simpleArraySum.py
@@ -1,5 +1,3 @@
-#!/bin/python3
-
 import os
 import sys
 
@@ -7,19 +5,9 @@ import sys
 # Complete the simpleArraySum function below.
 #
 def simpleArraySum(ar):
-    #
-    # Write your code here.
-    #
+    return sum(ar)
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ar_count = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
+    ar = [1, 2, 3, 4, 10, 11]
     result = simpleArraySum(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    print(result)
